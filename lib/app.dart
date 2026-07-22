70aed127d2ef467915493f521bf564a05702d7c1
kiruhak-MAC <kiruhak1991@gmail.com>
2026-07-22 23:44:16 +0700

    Base_project

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:crypto_app/router/router.dart';
+import 'package:crypto_app/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Crypto App', theme: darkTheme, routes: routes);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
-import 'package:crypto_app/router/router.dart';
-import 'package:crypto_app/theme/theme.dart';
+import 'package:crypto_app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const CryptoApp());
-}
-
-class CryptoApp extends StatelessWidget {
-  const CryptoApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Crypto App', theme: darkTheme, routes: routes);
-  }
 }
